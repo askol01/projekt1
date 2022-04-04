a91e7bf5bd05fc67f34b8298648c7470e4f2a5b0
Aleksandra Skolimowska <aleksandra.skolimowska.stud@pw.edu.pl>
2022-04-04 22:32:24 +0200

    zmodyfikowano

--- projekt.py
+++ projekt.py
@@ -11,5 +11,19 @@ from proj import *
 plik = "wsp_inp.txt"
 tablica = np.genfromtxt(plik, delimiter=',', skip_header = 4)
 
-T = Transformacje()
+T = Transformacje(model = "grs80")
+
+blh = []
+u92 = []
+for i in range(len(tablica)):
+    b, l, h = T.xyz2blh_hirvonen(tablica[i][0], tablica[i][1], tablica[i][2])
+    x92, y92 = T.u1992(b, l)
+    
+    blh.append([b, l, h])
+    u92.append([x92, y92])
+    
+
+wsp_out = np.hstack([blh, u92])    
+np.savetxt("wsp_out.txt", wsp_out, delimiter=',  ', fmt = ['%10.2f', '%10.2f', '%10.3f', '%10.3f', '%10.3f'], header = 'Konwersja współrzędnych geodezyjnych \\ Aleksandra Skolimowska \n blh      układ 1992')
+  
 
